468c51ac1507a502d08fec6ca1307d2843c64dc4
Tomasz Pajor <tomek@polishgeeks.com>
2017-08-09 11:04:18 +0200

    add tests initializer

---
+++ castle/test/__init__.py
@@ -0,0 +1,29 @@
+import unittest
+import pkgutil
+import logging
+import sys
+
+
+# The unittest module got a significant overhaul
+# in 2.7, so if we're in 2.6 we can use the backported
+# version unittest2.
+if sys.version_info[:2] == (2, 6):
+    import unittest2 as unittest
+else:
+    import unittest
+
+
+# Python 3 includes mocking, while 2 requires an extra module.
+if sys.version_info[0] == 2:
+    import mock
+else:
+    from unittest import mock
+
+TEST_MODULES = [
+    'castle.test.headers_formatter_test',
+    'castle.test.extractors.headers_test'
+]
+
+def all():
+    logging.basicConfig(stream=sys.stderr)
+    return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)

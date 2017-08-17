22a321a4a2017125a6406bb4a4266f4fc880c759
Tomasz Pajor <tomek@polishgeeks.com>
2017-08-17 21:48:15 +0200

    introduce Response

---
+++ castle/response.py
@@ -0,0 +1,29 @@
+from castle.exceptions import BadRequestError, UnauthorizedError, ForbiddenError, NotFoundError, UserUnauthorizedError, InvalidParametersError, ApiError
+
+RESPONSE_ERRORS = {
+    400: BadRequestError,
+    401: UnauthorizedError,
+    403: ForbiddenError,
+    404: NotFoundError,
+    419: UserUnauthorizedError,
+    422: InvalidParametersError
+}
+
+class Response(object):
+    def __init__(self, response):
+        self.response = response
+
+    def call(self):
+        if self.response.text is None or self.response.text == '':
+            return {}
+
+        self.verify()
+
+        return self.response.json()
+
+    def verify(self):
+        if self.response.status_code >= 200 and self.response.status_code <= 299:
+            return
+
+        error = RESPONSE_ERRORS.get(self.response.status_code, ApiError)
+        raise error(self.response.text)

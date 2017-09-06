4165aa9a0a4fc7052244ca53045e9176d2a4dead
Tomasz Pajor <tomek@polishgeeks.com>
2017-09-06 14:03:13 +0200

    add support for django WSGIRequest

--- castle/extractors/ip.py
+++ castle/extractors/ip.py
@@ -3,4 +3,17 @@ class ExtractorsIp(object):
         self.request = request
 
     def call(self):
-        return self.request.ip
+        if hasattr(self.request, 'ip'):
+            return self.request.ip
+        else:
+            return self.wsgi_request_ip()
+
+    def wsgi_request_ip(self):
+        x_forwarded_for = self.request.environ.get('HTTP_X_FORWARDED_FOR')
+
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = self.request.environ.get('REMOTE_ADDR')
+
+        return ip

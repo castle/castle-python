809e764f4b44023acd75fa6ac7370291f7618fd1
Tomasz Pajor <tomek@polishgeeks.com>
2017-10-15 17:14:19 +0200

    bring back request timeout

--- castle/request.py
+++ castle/request.py
@@ -13,7 +13,7 @@ class Request(object):
             method,
             self.build_url(endpoint),
             auth=('', configuration.api_secret),
-            # timeout=configuration.request_timeout,
+            timeout=configuration.request_timeout,
             headers=self.headers,
             verify=Request.verify(),
             data=None if params is None else json.dumps(params)

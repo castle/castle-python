1afcf30262a5964d2132e1093e79152f3d847075
Johanna Larsson <johanna@castle.io>
2019-01-09 10:01:19 +0100

    just checking to see if this helps dependabot

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ except ImportError:
 from castle.version import VERSION
 
 
+INSTALL_REQUIRES = [
+    'requests>=2.5',
+]
+
 if sys.version_info[:2] == (2, 6):
-    tests_require = ['responses<0.11', 'unittest2']
+    TESTS_REQUIRE = ['responses<0.11', 'unittest2']
 else:
-    tests_require = ['responses']
+    TESTS_REQUIRE = ['responses']
 
 setup(
     name="castle",
@@ -38,9 +42,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    install_requires=[
-        'requests>=2.5',
-    ],
-    tests_require=tests_require,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
     test_suite='castle.test.all'
 )

f3bea23321a22299fc92a4a1182d777d4d338979
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2020-02-05 15:08:58 +0100

    Drop special IP header handling

--- castle/extractors/ip.py
+++ castle/extractors/ip.py
@@ -6,17 +6,4 @@ class ExtractorsIp(object):
         if hasattr(self.request, 'ip'):
             return self.request.ip
 
-        return self.wsgi_request_ip()
-
-    def wsgi_request_ip(self):
-        cf_ip = self.request.environ.get('HTTP_CF_CONNECTING_IP')
-
-        if cf_ip:
-            return cf_ip.split(',')[0]
-
-        x_forwarded_for = self.request.environ.get('HTTP_X_FORWARDED_FOR')
-
-        if x_forwarded_for:
-            return x_forwarded_for.split(',')[0]
-
         return self.request.environ.get('REMOTE_ADDR')

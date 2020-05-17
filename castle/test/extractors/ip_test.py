1b534bfe0a444f2bc909f74d36d7793a40cad00c
marysieek <maria.korlotian@castle.io>
2020-05-17 18:32:17 +0200

    Add corresponding test cases

--- castle/test/extractors/ip_test.py
+++ castle/test/extractors/ip_test.py
@@ -7,6 +7,8 @@ class ExtractorsIpTestCase(unittest.TestCase):
     def tearDown(self):
         configuration.ip_headers = []
         configuration.trusted_proxies = []
+        configuration.trust_proxy_chain = False
+        configuration.trusted_proxy_depth = None
 
     def test_extract_ip(self):
         headers = {'X-Forwarded-For': '1.2.3.5'}
@@ -36,6 +38,28 @@ class ExtractorsIpTestCase(unittest.TestCase):
         self.assertEqual(
             ExtractorsIp(headers).call(),
             '127.0.0.1'
+        )
+
+    def test_extract_ip_when_trust_proxy_chain(self):
+        xf_header = """
+           6.6.6.6,2.2.2.3,6.6.6.5
+        """
+        headers = {'Remote-Addr': '6.6.6.4', 'X-Forwarded-For': xf_header}
+        configuration.trust_proxy_chain = True
+        self.assertEqual(
+            ExtractorsIp(headers).call(),
+            '6.6.6.6'
+        )
+
+    def test_extract_ip_when_trust_proxy_depth(self):
+        xf_header = """
+           6.6.6.6,2.2.2.3,6.6.6.5
+        """
+        headers = {'Remote-Addr': '6.6.6.4', 'X-Forwarded-For': xf_header}
+        configuration.trusted_proxy_depth = 1
+        self.assertEqual(
+            ExtractorsIp(headers).call(),
+            '2.2.2.3'
         )
 
     def test_extract_ip_for_spoof_ip_attempt(self):

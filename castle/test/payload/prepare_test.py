af1662e73301ed48c4e4174d85dcc0090885fb17
marysieek <maria.korlotian@castle.io>
2020-12-01 12:02:15 +0100

    Update the version

--- castle/test/payload/prepare_test.py
+++ castle/test/payload/prepare_test.py
@@ -24,7 +24,7 @@ def ctx():
             'X-Forwarded-For': '217.144.192.112'
         },
         'ip': '217.144.192.112',
-        'library': {'name': 'castle-python', 'version': '4.0.0'},
+        'library': {'name': 'castle-python', 'version': '5.0.0'},
         'user_agent': 'test'
     }
 
